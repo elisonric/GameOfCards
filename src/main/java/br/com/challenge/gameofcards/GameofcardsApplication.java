07b0c1971f5458ef26992131d3d6fdef3d081add
elisonric <elison.ricardo@gmail.com>
2024-09-12 15:54:45 -0300

    Criação da estrutura base do projeto e banco de dados

---
+++ src/main/java/br/com/challenge/gameofcards/GameofcardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.gameofcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameofcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameofcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/gameofcards/GameofcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.gameofcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameofcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
