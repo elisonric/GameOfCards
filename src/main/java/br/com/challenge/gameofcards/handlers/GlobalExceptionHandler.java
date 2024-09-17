91c29fcdce59cc06176e85ee3c20b90e0dd93062
Elison lamim <32721367+elisonric@users.noreply.github.com>
2024-09-17 14:01:47 -0300

    Tratamento de exceptions

---
+++ src/main/java/br/com/challenge/gameofcards/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package br.com.challenge.gameofcards.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handlerValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> bodyError = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            bodyError.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(bodyError);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handlerNoSuchElementExceptions(NoSuchElementException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleGlobalExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro não mapeado: " + ex.getMessage());
+    }
+}
--- src/main/java/br/com/challenge/gameofcards/models/GamePayload.java
+++ src/main/java/br/com/challenge/gameofcards/models/GamePayload.java
@@ -1,13 +1,16 @@
 package br.com.challenge.gameofcards.models;
 
+import jakarta.validation.constraints.Min;
 import org.springframework.lang.NonNull;
 
 public class GamePayload {
 
     @NonNull
+    @Min(value = 2, message = "O numero de jogadores não pode ser menor do que 2")
     private Integer numberPlayers;
 
     @NonNull
+    @Min(value = 1, message = "O numero de cartas não pode ser menor do que 1")
     private Integer numberCardsPerPlayer;
 
     public GamePayload() {
