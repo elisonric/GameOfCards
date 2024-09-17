0053d9776357d6facd3168a16544d0974ceef08c
Elison lamim <32721367+elisonric@users.noreply.github.com>
2024-09-17 14:01:10 -0300

    Update GameWinnerRepository.java

--- src/main/java/br/com/challenge/gameofcards/repositories/GameWinnerRepository.java
+++ src/main/java/br/com/challenge/gameofcards/repositories/GameWinnerRepository.java
@@ -5,6 +5,9 @@ import br.com.challenge.gameofcards.entities.ids.GameWinnerId;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameWinnerRepository extends CrudRepository<GameWinner, GameWinnerId> {
+    List<GameWinner> findByIdGame(Long idGame);
 }
